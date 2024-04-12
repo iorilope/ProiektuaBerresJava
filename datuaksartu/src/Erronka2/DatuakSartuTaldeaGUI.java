b27976fcf63a29f2804a382deb5b56a3bdf55458
iorilope <146725975+iorilope@users.noreply.github.com>
2024-04-12 12:50:17 +0200

    Taldea Klasea sortuta eta javadoc eginda, taldea objetuan gordetzen dira datuak ere

--- datuaksartu/src/Erronka2/DatuakSartuTaldeaGUI.java
+++ datuaksartu/src/Erronka2/DatuakSartuTaldeaGUI.java
@@ -10,150 +10,167 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
-
 public class DatuakSartuTaldeaGUI {
 
-    /**  frame 4. */
-    private static JFrame frame4 = new JFrame("Talde lehiaketa berrien emaitzak sartzeko aukera");
-    
-    /** Taldearen Datuen Panela. */
-    private static JPanel datuak = new JPanel();
-    
-    /** Taldeak jokatutako Txapelketaren ID-a. TextField */
-    private static JTextField talde_txapelketa_id = new JTextField();
-    
-    /** Taldearen Kodea TextField */
-    private static JTextField taldekod = new JTextField();
-    
-    /** Txapelketan egindako denbora. TextField */
-    private static JTextField taldetxapdenbora = new JTextField();
-    
-    /** Sartutako datuak bistaratzeko TextArea */
-    private static JTextArea erregistroa = new JTextArea();
-    
-    /** Datuak erregistroa TextArean sartzeko botoia. */
-    private static JButton datuaksartu = new JButton("Datuak sartu");
-    
-    /** Fitxategia sortzeko Botoia */
-    private static JButton fitxategiasortu = new JButton("Fitxategia sortu");
+	/** frame 4. */
+	private static JFrame frame4 = new JFrame("Talde lehiaketa berrien emaitzak sartzeko aukera");
 
-    /**
-    * Interfaze grafiko bat erakusten du, ekipo-txapelketa batekin lotutako datuak bistaratzeko eta sartzeko.
-    * Interfazeak torneoaren IDa, ekipoaren kodea eta ekipoaren denbora sartzeko eta erakusteko eremuak barne hartzen ditu
-    * txapelketan eta erregistroan, baita datuak sartzeko eta fitxategiak sortzeko botoiak ere.
-    */
-    
-    public static void pantailaErakutsi() {
-    	
-    	datuak.setLayout(new GridLayout(5, 2, 10, 10));
-        datuak.setBorder(new EmptyBorder(10, 10, 10, 10));
+	/** Taldearen Datuen Panela. */
+	private static JPanel datuak = new JPanel();
 
-       
-        datuak.add(new JLabel("Talde Txapelketa ID:"));
-        datuak.add(talde_txapelketa_id);
-        datuak.add(new JLabel("Taldekodea:"));
-        datuak.add(taldekod);
-        datuak.add(new JLabel("Talde txapen denbora:"));
-        datuak.add(taldetxapdenbora);
-        datuak.add(new JLabel("Erregistroa:"));
-        datuak.add(erregistroa);
-        datuak.add(datuaksartu);
-        datuak.add(fitxategiasortu);
+	/** Taldeak jokatutako Txapelketaren ID-a. TextField */
+	private static JTextField talde_txapelketa_id = new JTextField();
 
- 
-    	datuaksartu.addActionListener(new ActionListener() {
-			String textuerregistroa="";
+	/** Taldearen Kodea TextField */
+	private static JTextField taldekod = new JTextField();
+
+	/** Txapelketan egindako denbora. TextField */
+	private static JTextField taldetxapdenbora = new JTextField();
+
+	/** Sartutako datuak bistaratzeko TextArea */
+	private static JTextArea erregistroa = new JTextArea();
+
+	/** Datuak erregistroa TextArean sartzeko botoia. */
+	private static JButton datuaksartu = new JButton("Datuak sartu");
+
+	/** Fitxategia sortzeko Botoia */
+	private static JButton fitxategiasortu = new JButton("Fitxategia sortu");
+
+	/**
+	 * Interfaze grafiko bat erakusten du, ekipo-txapelketa batekin lotutako datuak
+	 * bistaratzeko eta sartzeko. Interfazeak torneoaren IDa, ekipoaren kodea eta
+	 * ekipoaren denbora sartzeko eta erakusteko eremuak barne hartzen ditu
+	 * txapelketan eta erregistroan, baita datuak sartzeko eta fitxategiak sortzeko
+	 * botoiak ere.
+	 */
+
+	public static void pantailaErakutsi() {
+
+		datuak.setLayout(new GridLayout(5, 2, 10, 10));
+		datuak.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		datuak.add(new JLabel("Talde Txapelketa ID:"));
+		datuak.add(talde_txapelketa_id);
+		datuak.add(new JLabel("Taldekodea:"));
+		datuak.add(taldekod);
+		datuak.add(new JLabel("Talde txapen denbora:"));
+		datuak.add(taldetxapdenbora);
+		datuak.add(new JLabel("Erregistroa:"));
+		datuak.add(erregistroa);
+		datuak.add(datuaksartu);
+		datuak.add(fitxategiasortu);
+
+		/**
+		 * ActionListener "datuaksartu" botoirako. Erabiltzaileak botoian klik egiten
+		 * duenean aktibatzen da. ActionListener honek ekipo-txapelketarekin lotutako
+		 * testu-eremuetan sartutako datuak hartzen ditu (Txapelketaren ID, taldearen
+		 * kodea, taldearen denbora txapelketan), horiekin "Taldea" motako objektu bat
+		 * sortzen du. datuak eta "erregistroa" testu-eremura gehitzen ditu, tabulazioz
+		 * bereizitako lerro-formatuan, eta ondoren lerro-jauzi bat egiten du.
+		 */
+
+		datuaksartu.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				String txapelketaId = talde_txapelketa_id.getText();
+				String taldeKod = taldekod.getText();
+				String taldetxapDenbora = taldetxapdenbora.getText();
 
-				textuerregistroa += talde_txapelketa_id.getText()+"\t";
-				talde_txapelketa_id.setText(null);
-				//Txapelketa_ID kutxan gorde den datua hartu eta textu kutxa garbitu
+				Taldea taldea = new Taldea(txapelketaId, taldeKod, taldetxapDenbora);
 
-				textuerregistroa += taldekod.getText()+"\t";
-				taldekod.setText(null);
-				//Taldekod kutxan gorde den datua hartu eta textu kutxa garbitu
-
-				textuerregistroa += taldetxapdenbora.getText()+"\n";
-				taldetxapdenbora.setText(null);
-				//Taldetxapdenbora kutxan gorde den datua hartu eta textu kutxa garbitu
-
-				erregistroa.setText(textuerregistroa);
-				//Erregistroa textu area eguneratu gorde ditugun datuekin
-
+				String datosTaldea = taldea.getTxapelketaId() + "\t" + taldea.getTaldeKod() + "\t"
+						+ taldea.getTaldetxapDenbora() + "\n";
+				erregistroa.append(datosTaldea);
 			}
 		});
 
-        // Configurar el botón "Fitxategia sortu"
-    	fitxategiasortu.addActionListener(new ActionListener() {
+		/**
+		 * "Fitxategiasortu" botoirako ActionListener, erabiltzaileak botoian klik
+		 * egiten duenean aktibatzen da. ActionListener honek erabiltzaileari eskatzen
+		 * dio sar dezala fitxategia eta artxiboaren izena gorde nahi dituen ibilbide
+		 * osoan. Ibilbidea eta fitxategiaren izena eman ondoren, testu-fitxategi bat
+		 * sortzen du zehaztutako kokalekuan. eta idatzi "erregistroa" testu-arloaren
+		 * edukia sortu berri den artxiboan. Gero, mezu bat erakusten du erabiltzaileari
+		 * berresteko, fitxategia arrakastaz sortu dela adieraziz. Errorerik izanez
+		 * gero, inprimatu kontsolako salbuespenak.
+		 */
+
+		fitxategiasortu.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String fitxategihelbidea=	JOptionPane.showInputDialog("Sartu fitxategiaren helbide absolutua(Hemen gordeko da zure fitxategia)");
-				//Fitxategia non gordeko den galdetu eta helbide hori gorde
+				String fitxategihelbidea = JOptionPane
+						.showInputDialog("Sartu fitxategiaren helbide absolutua(Hemen gordeko da zure fitxategia)");
+				// Fitxategia non gordeko den galdetu eta helbide hori gorde
 
 				while (fitxategihelbidea == null || fitxategihelbidea.isEmpty()) {
 
-					fitxategihelbidea =JOptionPane.showInputDialog("Fitxategiari helbide absolutua jarri behar zaio!");
+					fitxategihelbidea = JOptionPane.showInputDialog("Fitxategiari helbide absolutua jarri behar zaio!");
 
 				}
-				//Kutxa bete gabe geratzen bada, berriro galdeuko du, helbide bat idatzi arte
+				// Kutxa bete gabe geratzen bada, berriro galdeuko du, helbide bat idatzi arte
 
 				File file = new File(fitxategihelbidea);
-				//Fitxategiraren helbidea gordetzen da
+				// Fitxategiraren helbidea gordetzen da
 
 				while (!file.exists()) {
 
-					fitxategihelbidea=JOptionPane.showInputDialog("Fitxategiaren helbidea ez da existitzen. Mesedez, sartu existitzen den helbide bat.");
+					fitxategihelbidea = JOptionPane.showInputDialog(
+							"Fitxategiaren helbidea ez da existitzen. Mesedez, sartu existitzen den helbide bat.");
 					file = new File(fitxategihelbidea);
 				}
-				//Helbide existitzen den egiaztaten da, hau ez bada existitzen begizta jarraitzen du
+				// Helbide existitzen den egiaztaten da, hau ez bada existitzen begizta
+				// jarraitzen du
 
-				String 	fitxategizena	= 	JOptionPane.showInputDialog("Sartu fitxategiaren izena");
-				//Fitxategiaren izena galdetzen da
+				String fitxategizena = JOptionPane.showInputDialog("Sartu fitxategiaren izena");
+				// Fitxategiaren izena galdetzen da
 
 				while (fitxategizena == null || fitxategizena.isEmpty() || fitxategizena == ".txt") {
 
-					fitxategizena =JOptionPane.showInputDialog("Fitxategiak izena eduki behar du!");
-					//Fitxategizena aldagaian ez bada ezer gorde mezu bat inprimatuko da eta beriz galdetuko du
+					fitxategizena = JOptionPane.showInputDialog("Fitxategiak izena eduki behar du!");
+					// Fitxategizena aldagaian ez bada ezer gorde mezu bat inprimatuko da eta beriz
+					// galdetuko du
 				}
 
-				File izena = new File(fitxategihelbidea,fitxategizena+".txt");
-				//Fitxategiaren izena eta helbidea gordetzen da
+				File izena = new File(fitxategihelbidea, fitxategizena + ".txt");
+				// Fitxategiaren izena eta helbidea gordetzen da
 
 				while (izena.exists()) {
 
-					fitxategizena =JOptionPane.showInputDialog("Fitxategiaren izena iada existitzen da, jarri beste bat.");
-					izena = new File(fitxategihelbidea,fitxategizena+".txt");
+					fitxategizena = JOptionPane
+							.showInputDialog("Fitxategiaren izena iada existitzen da, jarri beste bat.");
+					izena = new File(fitxategihelbidea, fitxategizena + ".txt");
 				}
-				//Fitxategi izen hori helbide horretan existitzen den bitartean, begizta jarraituko du
+				// Fitxategi izen hori helbide horretan existitzen den bitartean, begizta
+				// jarraituko du
 
-				String ruta = fitxategihelbidea+ File.separator + fitxategizena +".txt";
-				//Fitxategiaren helbide absolutua eta honen izena aldagai berdinean gordetzen da "\"-kin elkartuz
+				String ruta = fitxategihelbidea + File.separator + fitxategizena + ".txt";
+				// Fitxategiaren helbide absolutua eta honen izena aldagai berdinean gordetzen
+				// da "\"-kin elkartuz
 
-				try {	  
+				try {
 					BufferedWriter fitxategia = new BufferedWriter(new FileWriter(ruta));
 					fitxategia.write(erregistroa.getText());
-					//Helbide egokia hartu eta bertan fitxategia sortu
+					// Helbide egokia hartu eta bertan fitxategia sortu
 
 					fitxategia.close();
-					//Writter-a itxi
+					// Writter-a itxi
 
 					JOptionPane.showMessageDialog(null, "Fitxategia sortu da!!");
-					//Fitxategia ondo sortu den mezua inprimatu eta begizta amaitu
+					// Fitxategia ondo sortu den mezua inprimatu eta begizta amaitu
 
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
-				//Errore mezua inprimatu
+				// Errore mezua inprimatu
 			}
 		});
-        // Frame-a konfiguratu
-        frame4.getContentPane().setLayout(new BorderLayout());
-        frame4.getContentPane().add(datuak, BorderLayout.CENTER);
-        frame4.setSize(600, 400);
-        frame4.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame4.setVisible(true);
-    }
-    }
 
+		// Frame-a konfiguratu
+		frame4.getContentPane().setLayout(new BorderLayout());
+		frame4.getContentPane().add(datuak, BorderLayout.CENTER);
+		frame4.setSize(600, 400);
+		frame4.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame4.setVisible(true);
+	}
+}
---
+++ datuaksartu/src/Erronka2/Taldea.java
@@ -0,0 +1,73 @@
+
+package Erronka2;
+
+
+/**
+ * Taldea klasea
+ */
+public class Taldea {
+	
+	/** Txapelketaren ID-a */
+	private String txapelketaId;
+	
+	/** Taldearen Kodea */
+	private String taldeKod;
+	
+	/** Taldearen Txapelketa denbora */
+	private String taldetxapDenbora;
+
+	/**
+	 * Talde berri bat hasieratzen du
+	 *
+	 * @param txapelketaId, Txapelketaren ID-a
+	 * @param taldeKod, Taldearen Kodea
+	 * @param taldetxapDenbora, Taldearen denbora Txapelketan
+	 */
+	public Taldea(String txapelketaId, String taldeKod, String taldetxapDenbora) {
+		this.txapelketaId = txapelketaId;
+		this.taldeKod = taldeKod;
+		this.taldetxapDenbora = taldetxapDenbora;
+	}
+
+	/**
+	 * @return Taldearen Txapelketa_ID
+	 */
+	public String getTxapelketaId() {
+		return txapelketaId;
+	}
+
+	/**
+	 * @return Taldearen Kodea
+	 */
+	public String getTaldeKod() {
+		return taldeKod;
+	}
+
+	/**
+	 * @return the Taldeak Txapelketan egindako denbora
+	 */
+	public String getTaldetxapDenbora() {
+		return taldetxapDenbora;
+	}
+
+	/**
+	 * @param Taldearen txapelketa_id-a ezarri
+	 */
+	public void setTxapelketaId(String txapelketaId) {
+		this.txapelketaId = txapelketaId;
+	}
+
+	/**
+	 * @param Taldearen kodea ezarri
+	 */
+	public void setTaldeKod(String taldeKod) {
+		this.taldeKod = taldeKod;
+	}
+
+	/**
+	 * @param Taldeak txapelketan egindako denbora ezarri
+	 */
+	public void setTaldetxapDenbora(String taldetxapDenbora) {
+		this.taldetxapDenbora = taldetxapDenbora;
+	}
+}
